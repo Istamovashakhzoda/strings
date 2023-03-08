7d76a388eefbf3878c5347627f5acba3129eadad
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-03-08 20:09:56 +0300

    Add files via upload

---
+++ string methods/script.js
@@ -0,0 +1,98 @@
+"use script"
+
+/*const arr=[
+  ["monday", "tuesday","wednesday"],
+  [ 1, 2,3,4,5],
+  [true,false]
+];
+for (let i=0; i<arr.length;i++){
+  for(let j=0; j<arr[i].length; j++){
+    console.log(arr[i][i]);
+  }
+}
+//WHILE
+let=1;
+while(i <=10){
+  console.log(i);
+
+
+  if(i===6){
+    break;
+  }
+  i++
+}
+
+//STRING METHODS
+let les="Fronted developer";
+console.log(les.length);
+for(let i=0; i<les.length; i++){
+  console.log(les[i]);
+}
+
+console.log(les.indexOf("t"));
+console.log(les.lastIndexOf("p"));
+console.log(les.startsWith("Fron"));
+console.log(les.endsWith("loper"));
+console.log(les.slice(0,6));
+console.log(les.slice(-5,-7));
+console.log(les.substring(0,5));
+console.log(les.substr(0,7));
+console.log(les.replace("developer" , "React"));
+console.log(les.toUpperCase());
+console.log(les.toLowerCase());
+console.log(les.concat("", "extra word"));
+console.log(les.trim());
+console.log(les.charAt(7));
+console.log(les.join( ));
+console.log(les.join(""));
+console.log(les.join(" "));*/
+
+
+/*const arr=[
+  ["monday", "tuesday","wednesday"],
+  [ 1, 2,3,4,5],
+  [true,false]
+];
+for (let i=0; i<arr.length;i++){
+  for(let j=0; j<arr[i].length; j++){
+    console.log(arr[i][i]);
+  }
+}
+//WHILE
+let=1;
+while(i <=10){
+  console.log(i);
+
+
+  if(i===6){
+    break;
+  }
+  i++
+}*/
+
+//STRING METHODS
+let les="Fronted developer";
+/*console.log(les.length);
+for(let i=0; i<les.length; i++){
+  console.log(les[i]);
+}*/
+
+console.log(les.indexOf("t"));
+console.log(les.lastIndexOf("p"));
+console.log(les.startsWith("Fron"));
+console.log(les.endsWith("loper"));
+console.log(les.slice(0,6));
+console.log(les.slice(-5,-7));
+console.log(les.substring(0,5));
+console.log(les.substr(0,7));
+console.log(les.replace("developer" , "React"));
+console.log(les.toUpperCase());
+console.log(les.toLowerCase());
+console.log(les.concat("", "extra word"));
+console.log(les.trim());
+console.log(les.charAt(7));
+console.log(les.join());
+console.log(les.join(""));
+console.log(les.join(" "));
+
+
